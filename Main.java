e67518c60009ec3478fe7579761d7d6c7ffba3f3
uma2003 <mahe292003@gmail.com>
2023-10-10 19:10:22 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package collection;
+import java.util.*;
+public class Main {
+	public static void main(String args[])
+	{
+		ArrayList<String> list=new ArrayList<String>();
+		list.add("Queen");
+		list.add("King");
+		list.add("People");
+		list.add("Gentleman");
+		for(String obj:list)
+			System.out.println(obj);		
+	}
+
+}
